9a681a1fb4b84eb1492cfed7108aa6504772ff78
sonyphoebeb <sonyphoebebaree@gmail.com>
2026-07-22 21:34:02 +0530

    style: add CSS for sum of digits page, feat: bind HTML form with JavaScript

--- SumOfDigits.js
+++ SumOfDigits.js
@@ -1,9 +1,15 @@
-let input = 1234;
-let sum = 0;
-while (input > 0) {
-    let digit = input % 10;
-    sum += digit;
-    input = Math.floor(input / 10);
-}
+document.getElementById("sumForm").addEventListener("submit", function (event)
+ {
+    event.preventDefault();
 
-console.log(sum);
+    let input = document.getElementById("numberInput").value;
+    let sum = 0;
+
+    while (input > 0) {
+        let digit = input % 10;
+        sum += digit;
+        input = Math.floor(input / 10);
+    }
+
+    document.getElementById("result").innerText = "Sum of digits: " + sum;
+});
